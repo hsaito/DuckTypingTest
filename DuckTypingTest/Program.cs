356164a009d3e8ada29d2449577f780a145e093e
Hideki Saito <hidekis@gmail.com>
2017-03-12 01:40:58 -0800

    Initial commit

--- DuckTypingTest/Program.cs
+++ DuckTypingTest/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using static System.Console;
 
-namespace DuckTypingTest
+class Duck
 {
-    class Program
+    public void Quack() => WriteLine("Quaaaaaack!");
+    public void Feathers() => WriteLine("The duck has white and gray feathers.");
+}
+
+class Person
+{
+    public void Quack() => WriteLine("The person imitates a duck.");
+    public void Feathers() => WriteLine("The person takes a feather from the ground and shows it.");
+}
+
+class Program
+{
+    static void InTheForest(dynamic obj)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        obj.Quack();
+        obj.Feathers();
+    }
+
+    static void Game()
+    {
+        Duck donald = new Duck();
+        Person john = new Person();
+        InTheForest(donald);
+        InTheForest(john);
+    }
+
+    static void Main()
+    {
+        Game();
     }
 }
